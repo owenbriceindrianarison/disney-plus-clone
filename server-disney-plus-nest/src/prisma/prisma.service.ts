521ecf3a746dd0fbad00a447158924924b933c7d
Owen Brice INDRIANARISON <indrianarison.brice@gmail.com>
2022-01-03 16:23:06 +0100

    Prisma Service Setup

---
+++ server-disney-plus-nest/src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(private configService: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+
+  onModuleInit() {
+    this.$connect();
+  }
+
+  onModuleDestroy() {
+    this.$disconnect();
+  }
+}
